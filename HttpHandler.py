64d5da381880b98253e0ed99cfbb1076efb08d69
Johan Wirén <johan.wiren.se@gmail.com>
2013-08-16 22:49:23 +0200

    Playsafe..

--- HttpHandler.py
+++ HttpHandler.py
@@ -24,9 +24,9 @@ class HttpHandler(SimpleHTTPRequestHandler):
             s.end_headers()
             id = int(s.path.split('/')[2])
             result = dict()
-            result['command'] = s.server.hbq.commands[id].args
-            result['status'] = s.server.hbq.commands[id].status
-            result['stdout'] = s.server.hbq.commands[id].stdout
+            result['command'] = s.server.playsafe.commands[id].args
+            result['status'] = s.server.playsafe.commands[id].status
+            result['stdout'] = s.server.playsafe.commands[id].stdout
             s.wfile.write(json.dumps(result))
         elif joblist.match(s.path):
             s.send_response(200)
@@ -34,14 +34,18 @@ class HttpHandler(SimpleHTTPRequestHandler):
             s.end_headers()
             result = dict()
             for i, cmd in enumerate([[x.args, x.status]
-                                    for x in s.server.hbq.commands]):
+                                    for x in s.server.playsafe.commands]):
                 result[i] = dict(command=" ".join(cmd[0]), status=cmd[1])
             s.wfile.write(json.dumps(result))
         else:
             SimpleHTTPRequestHandler.do_GET(s)
 
     def do_POST(s):
-        command = s.rfile.read(int(s.headers['Content-Length']))
-        s.server.hbq.add(command.split())
-        s.send_response(200)
-        s.end_headers()
+        url = s.rfile.read(int(s.headers['Content-Length']))
+        try:
+            s.server.playsafe.add(url.rstrip().replace('url=', ''))
+            s.send_response(200)
+        except Exception, e:
+            s.send_response(500)
+            s.end_headers()
+            s.wfile.write(e)
---
+++ PlaySafe.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+from Command import Command
+from HttpHandler import HttpHandler
+from Queue import Queue
+from threading import Thread
+import BaseHTTPServer
+import json
+import os
+import re
+import sys
+import urllib
+import xml.etree.ElementTree as ElementTree
+import yaml
+
+FFMPEGARGS = [ "-vcodec", "copy", "-strict", "experimental", "-acodec", "copy", "-absf", "aac_adtstoasc" ]
+RESOLUTION = '1280x720'
+EXTENSION = 'mp4'
+
+class PlaySafe(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.files = []
+        self.commands = []
+        self.q = Queue()
+        self.run_threads = True
+        self.thread = Thread(target=self.runner)
+
+    def runner(self):
+        while self.run_threads:
+            command = self.q.get()
+            if command != 'stop_server':
+                command.run()
+
+    def get_filename(self, jsondata):
+        filename = jsondata['statistics']['folderStructure'].split('.')[0]
+        filename += '-'
+        filename += ''.join(jsondata['statistics']['title'].split(':')[0:2])
+        return filename
+
+    def get_streamurl(self, jsondata):
+        for vref in jsondata['video']['videoReferences']:
+            if vref['playerType'] == 'ios':
+                return vref['url']
+        return None
+
+    def get_stream_from_m3u8(self, m3u):
+        resolution_re = re.compile(r'.*RESOLUTION=' + RESOLUTION + r'.*')
+        wanted_line = None
+        for l in m3u.splitlines():
+            if wanted_line:
+                return l
+            if resolution_re.match(l):
+                wanted_line = True
+        return None
+
+    def add(self, url):
+        jsondata = json.loads(urllib.urlopen(urllib.unquote(url) + "?output=json").read())
+        m3u = urllib.urlopen(self.get_streamurl(jsondata)).read()
+        stream = self.get_stream_from_m3u8(m3u)
+        if stream is None:
+            return
+        args = [ "ffmpeg", "-i", stream ]
+        args += FFMPEGARGS
+        args.append(self.get_filename(jsondata) + "." + EXTENSION)
+        command = Command(args)
+        self.commands.append(command)
+        self.q.put(command)
+        return command
+
+    def stop_server(self):
+        self.run_threads = False
+        self.q.put('stop_server')
+        self.thread.join()
+
+    def run_server(self):
+        self.httpserver = HttpServer(('', self.config['port']), HttpHandler, self)
+        self.thread.start()
+        try:
+            self.httpserver.serve_forever()
+        except KeyboardInterrupt:
+            self.stop_server()
+
+class HttpServer(BaseHTTPServer.HTTPServer):
+
+    def __init__(self, address, handler, playsafe):
+        self.playsafe = playsafe
+        BaseHTTPServer.HTTPServer.__init__(self, address, handler)
+
+if __name__ == "__main__":
+    try:
+        config = yaml.load(open('config.yml'))
+    except:
+        print "Could not load config.yml"
+        sys.exit(1)
+    PlaySafe(config).run_server()
